f1d559580e0133358827264a081b192951c484ec
Gregor Heine <gheine@gilt.com>
2016-06-01 15:34:25 +0100

    Fix parameter type mismatch exception on nested parameterized types

--- src/main/scala/com/codahale/jerkson/Types.scala
+++ src/main/scala/com/codahale/jerkson/Types.scala
@@ -14,10 +14,12 @@ private[jerkson] object Types {
   private def constructType(factory: TypeFactory, manifest: Manifest[_]): JavaType = {
     if (manifest.erasure.isArray) {
       ArrayType.construct(factory.constructType(manifest.erasure.getComponentType), null, null)
-    } else {
+    } else if (manifest.erasure.getTypeParameters.size == 0 || manifest.typeArguments.size > 0) {
       factory.constructParametricType(
         manifest.erasure,
         manifest.typeArguments.map {m => build(factory, m)}.toArray: _*)
+    } else {
+      factory.constructType(manifest.erasure)
     }
   }
 }
